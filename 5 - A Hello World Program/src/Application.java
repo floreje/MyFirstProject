643fd8b3daf8bf4bbfa2413d64f9953e24b05181
Jerica Flores <jerica.flores@oocl.com>
2016-06-13 17:03:42 +0800

    D01 JAVA exercises

---
+++ 5 - A Hello World Program/src/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+ 
+    public static void main(String[] args) {
+    /*	int value = 0; //initialize part
+    	
+    	do{
+    		System.out.println("value = " + value);
+    		value++;
+    	}while (value < 10);*/
+        System.out.println("Hello World!");
+    }
+ 
+}
